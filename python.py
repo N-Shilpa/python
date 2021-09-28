f79c2a67fe834784556e0da8bfd0856aabe82ef7
n shilpa <shilu969@gmail.com>
2021-09-28 13:07:52 +0530

    files are addeed

---
+++ python.py
@@ -0,0 +1,15 @@
+height=2.3
+weight=65.8
+bmi=weight/height**2
+print("bmi=",bmi)
+
+no1 = int(input("Enter no1"))
+no2= int(input ("Enter no2"))
+if(no1 < no2):
+    sum  = no1+no2
+    print(sum)
+elif(no1> 5):
+    sub = no1-no2
+    print(sub)
+else:
+    print("Hello")
