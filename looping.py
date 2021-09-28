f1a9ef572061a5205d0fc37124e9beedd402d78c
n shilpa <shilu969@gmail.com>
2021-09-28 15:25:59 +0530

    files are added

---
+++ looping.py
@@ -0,0 +1,13 @@
+for i in range (5):
+    print("shilpa")
+
+for i in range(5,10):
+    print("python")
+
+list=[1,2,3,4,"python"]
+for i in list:
+    print(i)
+
+words=['bat','hello','world']
+for w in words:
+    print(w,len(w))
